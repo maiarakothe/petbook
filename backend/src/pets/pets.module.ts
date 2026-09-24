56f03e2a9038f18504b29664ce44b7fdc67da34a
maiarakothe <maiarakothe0@gmail.com>
2026-09-23 23:11:30 -0300

    fix: ajusta para rodar

--- backend/src/pets/pets.module.ts
+++ backend/src/pets/pets.module.ts
@@ -4,13 +4,11 @@ import { PetsController } from './pets.controller';
 import { PetsService } from './pets.service';
 import { DatabaseModule } from '../database/database.module';
 import { AuthModule } from '../auth/auth.module';
-import { CloudinaryModule } from '../cloudinary/cloudinary.module';
 
 @Module({
   imports: [
     DatabaseModule,
     AuthModule,
-    CloudinaryModule,
   ],
   controllers: [
     PetsController,
